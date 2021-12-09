de6b2b17cffa91f68e379f884995f668ce51fb38
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-09 12:47:01 +0300

    Serve Login component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+//style
 import './App.css';
 
+//Npm packages
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+//components
+import Login from './components/Login';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
