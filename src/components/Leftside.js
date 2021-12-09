514277c49fc040b77b49ed5bc3032dfdd66f9167
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-09 12:10:11 +0300

    Create component files.

---
+++ src/components/Leftside.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Leftside = () => {
+  return <div>leftside</div>;
+};
+
+export default Leftside;
---
+++ src/components/Rightside.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Rightside = () => {
+  return <div>rightside</div>;
+};
+
+export default Rightside;
