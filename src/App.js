358a1c86c5413c1e7d374d81477f8f167fcac5bb
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-09 16:03:43 +0300

    Serve Header, Home components.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 //components
 import Login from './components/Login';
+import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Login />
+          </Route>
+          <Route exact path="/home">
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </Router>
