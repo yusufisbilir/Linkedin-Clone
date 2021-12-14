474e74461c0062966d2938cdaecdc49ba38fc39b
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-14 20:38:35 +0300

    Create right side of main page.

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -1,11 +1,129 @@
 import styled from 'styled-components';
 
 const Rightside = () => {
-  return <Container>right</Container>;
+  return (
+    <Container>
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+          <img src="/images/feed-icon.svg" alt="feed" />
+        </Title>
+
+        <FeedList>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </FeedList>
+
+        <Recommendation>
+          View all recommendations
+          <img src="/images/right-icon.svg" alt="right" />
+        </Recommendation>
+      </FollowCard>
+      <BannerCard>
+        <img
+          src="https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg"
+          alt=""
+        />
+      </BannerCard>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   grid-area: rightside;
+`;
+
+const FollowCard = styled.div`
+  background-color: #fff;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+  margin-bottom: 8px;
+  overflow: hidden;
+  padding: 12px;
+  position: relative;
+  text-align: center;
+`;
+
+const Title = styled.div`
+  align-items: center;
+  color: rgba(0, 0, 0, 0.6);
+  display: inline-flex;
+  font-size: 16px;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const FeedList = styled.ul`
+  margin-top: 16px;
+  li {
+    align-items: center;
+    display: flex;
+    font-size: 14px;
+    margin: 12px 0;
+    position: relative;
+    & > div {
+      display: flex;
+      flex-direction: column;
+    }
+
+    button {
+      align-items: center;
+      background-color: transparent;
+      border-radius: 15px;
+      box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.6);
+      box-sizing: border-box;
+      color: rgba(0, 0, 0, 0.6);
+      display: inline-flex;
+      font-weight: 600;
+      max-height: 32px;
+      max-width: 480px;
+      padding: 16px;
+      text-align: center;
+      justify-content: center;
+      outline: none;
+    }
+  }
+`;
+
+const Avatar = styled.div`
+  background-image: url('https://static-exp1.licdn.com/sc/h/1b4vl1r54ijmrmcyxzoidwmxs');
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 48px;
+  height: 48px;
+  margin-right: 8px;
+`;
+
+const Recommendation = styled.a`
+  color: #0a66c2;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+`;
+
+const BannerCard = styled(FollowCard)`
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 export default Rightside;
