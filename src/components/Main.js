434b7f592ceff319a31a7d3b4913a7e3a19e37d2
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-15 16:31:13 +0300

    Create share box.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,107 @@
 import styled from 'styled-components';
 
 const Main = () => {
-  return <Container>main</Container>;
+  return (
+    <Container>
+      <ShareBox>
+        <div>
+          <img src="/images/user.svg" alt="user" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" alt="add" />
+            <span>Photo</span>
+          </button>
+
+          <button>
+            <img src="/images/video-icon.svg" alt="add" />
+            <span>Video</span>
+          </button>
+
+          <button>
+            <img src="/images/event-icon.svg" alt="add" />
+            <span>Event</span>
+          </button>
+
+          <button>
+            <img src="/images/article-icon.svg" alt="add" />
+            <span>Write a article</span>
+          </button>
+        </div>
+      </ShareBox>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  background-color: #fff;
+  border-radius: 5px;
+  border: none;
+  box-shadow: rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+  margin-bottom: 8px;
+  overflow: hidden;
+  position: relative;
+  text-align: center;
+`;
+
+const ShareBox = styled(CommonCard)`
+  background: #fff;
+  color: #958b7b;
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 8px;
+  div {
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+
+      button {
+        img {
+          margin: 0 4px 0 -2px;
+        }
+        span {
+          color: #70b5f9;
+        }
+      }
+    }
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        border-radius: 35px;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        background-color: #fff;
+        text-align: left;
+      }
+    }
+  }
 `;
 
 export default Main;
